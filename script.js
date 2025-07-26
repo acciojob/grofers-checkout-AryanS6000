a2e92160a9061173f852bbfd8089e0bf8aaeedd8
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-07-26 23:04:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+ const sum =  document.Element.querySelectorAll("price");
+	let total = 0;
+	sum.foreach(function(priceElement){
+	 const price = parsefloat(priceElement.textContent);
+		total = price+total;
+	});
+	const newrow = document.createElement("tr");
+	const newcell = document.createElement("td");
+	newcell.setAttribute("colspan",2);
+	newcell.textContent = "total-price:" + total;
+	const table = document.querySelector(table);
+	newrow.appendChild(newcell);
+	table.appendChild(newrow);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
+
+
+
+
+
+
+
+
 
