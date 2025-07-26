6ddd40483c85f2f482d45062b5b29725043cfe86
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-07-26 23:15:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
- const sum =  document.querySelectorAll("price");
+ const sum =  document.querySelectorAll(".price");
 	let total = 0;
 	sum.forEach(function(priceElement){
 	 const price = parseFloat(priceElement.textContent);
@@ -12,7 +12,7 @@ const getSum = () => {
 	});
 	const newrow = document.createElement("tr");
 	const newcell = document.createElement("td");
-	newcell.setAttribute("colspan","2");
+	newcell.setAttribute("id", "ans");
 	newcell.textContent = "total-price:" + total;
 	const table = document.querySelector("table");
 	newrow.appendChild(newcell);
